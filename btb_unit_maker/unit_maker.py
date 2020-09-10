8fc0f094f57be8964f011f3527f2926a863bae49
squidboy3999 <58859289+squidboy3999@users.noreply.github.com>
2020-09-10 14:04:17 -0700

    Create unit_maker.py
    initial GameUnit class.

---
+++ btb_unit_maker/unit_maker.py
@@ -0,0 +1,14 @@
+class GameUnit:
+    unit_dict={}
+    
+    def __init__(self):
+        pass
+        
+    def reload_from_dict(self,unit_dict):
+        self.unit_dict=unit_dict
+    
+    def create_type(self,type,level,team):
+        pass
+        
+    def inc_level(self):
+        pass
